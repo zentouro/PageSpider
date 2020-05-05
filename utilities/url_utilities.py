820acc65868a3ce7d56d7e3755720616667aafaf
zentouro <miriam.nielsen@columbia.edu>
2020-05-04 22:19:49 -0400

    initial commit

---
+++ utilities/url_utilities.py
@@ -0,0 +1,14 @@
+def load_urls_from_file(file_path: str):
+    #TODO: add the code needed to read the text file with the urls in it
+    pass
+
+
+def load_page(url: str):
+    #TODO: add the code that reads the URL
+    pass
+
+def scrape_page(page_contents: str):
+    #TODO: analyze the text
+    pass
+
+
